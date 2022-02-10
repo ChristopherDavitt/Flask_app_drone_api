c29a3e9e2609bd3729309c2d2e01e272b2390ed3
Chris Davitt <ChrisDavitt3@gmail.com>
2022-02-10 12:16:48 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# gives access to the project in any OS we find mac, pc, etc.
+# allows outside files/folders to be added to the project from the base directory
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    # FLASK_ENV
+    """
+    Set Config variables for the flask app/
+    Using envoironment variables where available otherwise
+    create the config variable if not done
+    
+    """
+    
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess.. NA NA nana BOO BOO'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # turn off update messages from sql
